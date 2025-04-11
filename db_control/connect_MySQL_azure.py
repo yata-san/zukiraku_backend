b58a0520be9746b5de499a205e39fe7813c4e924
KyokoNakamura-ch <kyoko.nakamura.2021@globis.ac.jp>
2025-04-11 09:09:49 +0900

    Add files via upload

---
+++ db_control/connect_MySQL_azure.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+import os
+
+# Azure接続情報
+DATABASE_URL = "mysql+pymysql://tech0gen9student:vY7JZNfU@rdbs-002-step3-2-oshima1.mysql.database.azure.com:3306/crm_mysql"
+
+# 証明書のパス
+SSL_CA_PATH = "C:/Users/herim/Desktop/DigiCertGlobalRootCA.crt.pem"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"ssl": {"ca": SSL_CA_PATH}}
+)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
